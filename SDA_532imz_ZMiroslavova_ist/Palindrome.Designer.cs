e68de8cf231eb64b5a9edac6ee1df21929082a6a
zmiroslavova <zmiroslavova@gmail.com>
2018-05-07 18:51:41 +0300

    Add project files.

---
+++ SDA_532imz_ZMiroslavova_ist/Palindrome.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SDA_532imz_ZMiroslavova_ist
+{
+    partial class Palindrome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_InputNumber = new System.Windows.Forms.Label();
+            this.textBox_inputNimber = new System.Windows.Forms.TextBox();
+            this.button_Result = new System.Windows.Forms.Button();
+            this.richTextBox_Result = new System.Windows.Forms.RichTextBox();
+            this.button_Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_InputNumber
+            // 
+            this.label_InputNumber.AutoSize = true;
+            this.label_InputNumber.Location = new System.Drawing.Point(13, 9);
+            this.label_InputNumber.Name = "label_InputNumber";
+            this.label_InputNumber.Size = new System.Drawing.Size(79, 13);
+            this.label_InputNumber.TabIndex = 0;
+            this.label_InputNumber.Text = "Enter a number";
+            // 
+            // textBox_inputNimber
+            // 
+            this.textBox_inputNimber.Location = new System.Drawing.Point(98, 2);
+            this.textBox_inputNimber.Name = "textBox_inputNimber";
+            this.textBox_inputNimber.Size = new System.Drawing.Size(167, 20);
+            this.textBox_inputNimber.TabIndex = 1;
+            // 
+            // button_Result
+            // 
+            this.button_Result.Location = new System.Drawing.Point(97, 34);
+            this.button_Result.Name = "button_Result";
+            this.button_Result.Size = new System.Drawing.Size(167, 34);
+            this.button_Result.TabIndex = 2;
+            this.button_Result.Text = "Result";
+            this.button_Result.UseVisualStyleBackColor = true;
+            this.button_Result.Click += new System.EventHandler(this.button_Result_Click);
+            // 
+            // richTextBox_Result
+            // 
+            this.richTextBox_Result.Location = new System.Drawing.Point(12, 79);
+            this.richTextBox_Result.Name = "richTextBox_Result";
+            this.richTextBox_Result.ReadOnly = true;
+            this.richTextBox_Result.Size = new System.Drawing.Size(252, 84);
+            this.richTextBox_Result.TabIndex = 3;
+            this.richTextBox_Result.Text = "";
+            // 
+            // button_Clear
+            // 
+            this.button_Clear.Location = new System.Drawing.Point(97, 177);
+            this.button_Clear.Name = "button_Clear";
+            this.button_Clear.Size = new System.Drawing.Size(167, 34);
+            this.button_Clear.TabIndex = 4;
+            this.button_Clear.Text = "Clear";
+            this.button_Clear.UseVisualStyleBackColor = true;
+            this.button_Clear.Click += new System.EventHandler(this.button_Clear_Click);
+            // 
+            // Palindrome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 220);
+            this.Controls.Add(this.button_Clear);
+            this.Controls.Add(this.richTextBox_Result);
+            this.Controls.Add(this.button_Result);
+            this.Controls.Add(this.textBox_inputNimber);
+            this.Controls.Add(this.label_InputNumber);
+            this.Name = "Palindrome";
+            this.Text = "Palindrome";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_InputNumber;
+        private System.Windows.Forms.TextBox textBox_inputNimber;
+        private System.Windows.Forms.Button button_Result;
+        private System.Windows.Forms.RichTextBox richTextBox_Result;
+        private System.Windows.Forms.Button button_Clear;
+    }
+}
+
---
+++ SDA_532imz_ZMiroslavova_ist/Palindrome.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDA_532imz_ZMiroslavova_ist
+{
+    public partial class Palindrome : Form
+    {
+        public Palindrome()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Result_Click(object sender, EventArgs e)
+        {
+            int inputNumber = int.Parse(textBox_inputNimber.Text);
+            int sum = inputNumber + reverseNumber(inputNumber);
+
+            string output = "";
+            if (isPalindrome(sum))
+            {
+                output = "Sum of" + inputNumber + " and his reverse number is " + sum + " and it is palindrom";
+            }
+            else
+            {
+                while (sum < 1000000)
+                {
+                    sum = sum + reverseNumber(sum);
+                    if (isPalindrome(sum))
+                    {
+                        output = "The final sum of " + inputNumber + " and his reverse numbers is " + sum + " and it is palindrom";
+                        break;
+                    }
+                    else
+                    {
+                        output = inputNumber + " is special number";
+                    }
+                }
+
+            }
+            this.richTextBox_Result.Text = output;
+        }
+        
+        public int reverseNumber(int inputNumber)
+        {
+            int left = inputNumber;
+            int revNumber = 0;
+
+            while(left>0)
+            {
+                int r = left % 10;
+                revNumber = revNumber * 10 + r;
+                left = left / 10;
+            }
+            return revNumber;
+        }
+
+        private Boolean isPalindrome(int number)
+        {
+            Boolean isPalindrome = false;
+            char[] num = number.ToString().ToCharArray();
+            char[] reversed = number.ToString().ToCharArray();
+            Array.Reverse(reversed);
+
+            String numberString = new String(num);
+            String reversedNumber = new String(reversed);
+            
+            if (numberString == reversedNumber)
+                
+                isPalindrome = true;
+            return isPalindrome;
+        }
+
+        private void button_Clear_Click(object sender, EventArgs e) => clear();
+       
+        void clear()
+        {
+            textBox_inputNimber.Clear();
+            richTextBox_Result.Clear();
+
+            textBox_inputNimber.Focus();
+        }
+
+        
+    }
+}
